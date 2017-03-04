fad6b9a9dab4a754def942c6843590de112f77bf
huynh do ngoc <ngochuynh1991@gmail.com>
2017-03-04 17:28:25 +0700

    Add PORT agan

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.post('/', (req, res) => {
   BOT_CHAT(req, res);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening on port ', port);
 });
